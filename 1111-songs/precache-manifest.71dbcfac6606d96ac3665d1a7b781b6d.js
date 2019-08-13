e195b6ce2b5ad319df9f4345f8c91e7d85d40f21
Plamen Kolev <p.kolev22@gmail.com>
2019-08-13 23:16:09 +0100

    added static 1111 project to github pages

---
+++ 1111-songs/precache-manifest.71dbcfac6606d96ac3665d1a7b781b6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ded3802b67c4f6e20ebd7b171e0b92e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c18a78b459d9bf8d6f13",
+    "url": "/static/css/main.fda83529.chunk.css"
+  },
+  {
+    "revision": "edf7948371ec66bb1ea4",
+    "url": "/static/js/2.757abbdb.chunk.js"
+  },
+  {
+    "revision": "c18a78b459d9bf8d6f13",
+    "url": "/static/js/main.8be2b42e.chunk.js"
+  },
+  {
+    "revision": "95c084e112b9aa34cfb9",
+    "url": "/static/js/runtime~main.e06b9d57.js"
+  }
+]);
